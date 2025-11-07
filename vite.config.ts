d07e4ceb4e5bd975ebeea179550ec833320c999c
Rudin Engineer <rudinengineer@gmail.com>
2025-11-07 19:04:27 +0700

    update basepath

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/music-player/",
+  base: "/",
   plugins: [vue(), tailwindcss()],
 });
